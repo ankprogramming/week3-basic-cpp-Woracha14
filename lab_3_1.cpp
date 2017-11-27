d127d367aad217c1e26b7b983ad1e601e3a42352
Unknown <milk756@hotmail.com>
2017-11-27 18:10:44 +0700

    ทำข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss. Woracha Phadphong" << endl;
+  cout << "Nickname Milk" << endl;
+  cout << "Sex Female" << endl;
+  cout << "Birthday December 13,1998" << endl;
+  cout << "Status Single" << endl;
   return 0;
 }
