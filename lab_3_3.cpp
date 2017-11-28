8a074ad1006926585389a59065e76a6bee00b4e9
Unknown <milk756@hotmail.com>
2017-11-28 11:38:08 +0700

    ทำข้อ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-  
+    const float pi = 3.14315926536;
+    float r;
+    float h;
+
+    cout << "Please enter radius : ";
+    cin >> r;
+
+    cout << "Area of Circle : " << pi * r * r << endl;
+    cout << "line of Circle : " << 2 * pi * r * r << endl;
+
+    cout << "Please enter high : ";
+    cin >> h;
+    cout << "Area of Cylinder : " << pi * r * r * h << endl;
+
+
+
+
   return 0;
 }
